8d994daccf8b3d81d61781d8282e14ecb4c08cb8
manoj0996 <87071574+manoj0996@users.noreply.github.com>
2021-08-24 20:13:16 +0100

    Update Category.swift

--- Granny/Category.swift
+++ Granny/Category.swift
@@ -2,8 +2,7 @@
 //  Category.swift
 //  My Granny
 //
-//  Created by Sedenion technologies on 07/05/2020.
-//  Copyright © 2020 Sedenion technologies. All rights reserved.
+// created by Manoj Challagunda
 //
 
 import Foundation
