4bcde60d59006438c5e53368b9adec5aa35aef2d
manoj0996 <87071574+manoj0996@users.noreply.github.com>
2021-08-24 20:16:19 +0100

    Update MessagesTableViewController.swift

--- Granny/MessagesTableViewController.swift
+++ Granny/MessagesTableViewController.swift
@@ -2,8 +2,7 @@
 //  MessagesTableViewController.swift
 //  My Granny
 //
-//  Created by Sedenion technologies on 18/04/2020.
-//  Copyright © 2020 Sedenion technologies. All rights reserved.
+// created by Manoj Challagunda
 //
 
 import Foundation
