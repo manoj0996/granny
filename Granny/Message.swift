2b351590c4d9460f727a140dc954c18eddc09e33
manoj0996 <87071574+manoj0996@users.noreply.github.com>
2021-08-24 20:15:33 +0100

    Update Message.swift

--- Granny/Message.swift
+++ Granny/Message.swift
@@ -2,8 +2,7 @@
 //  Message.swift
 //  My Granny
 //
-//  Created by Sedenion technologies on 11/05/2020.
-//  Copyright © 2020 Sedenion technologies. All rights reserved.
+// created by Manoj Challagunda
 //
 
 import Foundation
